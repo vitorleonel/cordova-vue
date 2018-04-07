196939f9093be0f4f0fd116b4bdf9851b990afb2
Vitor Leonel <falecom@vitorleonel.com>
2018-04-07 04:02:58 -0300

    vuex

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,11 @@ import '@/scss/app.scss'
 import Vue from 'vue'
 import App from './components/App'
 import router from './router'
+import store from './store'
 
 new Vue({
     el: '#app',
     router,
+    store,
     render: h => h(App)
 })
